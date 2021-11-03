119838723ffbd712c654454fed2f6119e79ed337
Rhea-Roxy <79743359+Rhea-Roxy@users.noreply.github.com>
2021-11-03 19:44:46 -0400

    New

--- .idea/CODES/DNAComplement.java
+++ .idea/CODES/DNAComplement.java
@@ -1,29 +1,48 @@
-package com.coding;
+package com.company;
+
+import java.util.Scanner;
 
 public class DNAComplement {
     public static void main(String[] args) {
 
-        DNAComplement y = new DNAComplement();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the dna pattern: ");
-        String dna_string = sc.next();
-        System.out.println(y.complement(dna_string));
+        System.out.println("Enter the dna sequence : ");
+        String dna = sc.next();
+        DNAComplement y = new DNAComplement();
+        System.out.println(y.complement(dna));
     }
+
     public String complement(String dna) {
-        String new_dna="";
-        for(int i = 0; i< dna.length(); i++)
-        {
-            switch(dna.charAt(i)){
-                case 'g' : new_dna += 'c';
+        String new_dna = "";
+        for (int i = 0; i < dna.length(); i++) {
+            switch (dna.charAt(i)) {
+                case 'g':
+                    new_dna += 'c';
                     break;
-                case 'c' : new_dna += 'g';
+                case 'c':
+                    new_dna += 'g';
                     break;
-                case 't' : new_dna += 'a';
+                case 't':
+                    new_dna += 'a';
                     break;
-                case 'a' : new_dna += 't';
+                case 'a':
+                    new_dna += 't';
                     break;
-                default :
-                    System.out.println("The dna sequence is wrong"); System.exit(0);
+                case 'G':
+                    new_dna += 'C';
+                    break;
+                case 'C':
+                    new_dna += 'g';
+                    break;
+                case 'T':
+                    new_dna += 'A';
+                    break;
+                case 'A':
+                    new_dna += 'T';
+                    break;
+                default:
+                    System.out.println("The dna sequence is wrong");
+                    System.exit(0);
             }
         }
         return new_dna;
--- .idea/CODES/DNAReverse.java
+++ .idea/CODES/DNAReverse.java
@@ -1,23 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class DNAReverse {
     public static void main(String[] args) {
         DNAReverse y = new DNAReverse();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the dna pattern: ");
-        String dna_string = sc.next();
-        System.out.println(y.reverse(dna_string));
+        String string = sc.next();
+        System.out.println(y.reverse(string));
     }
+
     public String reverse(String dna) {
-        String new_dna="";
-        for(int i = dna.length()-1; i >= 0; i--)
-        {   if(dna.charAt(i)=='t'||dna.charAt(i)=='a'||dna.charAt(i)=='g'||dna.charAt(i)=='c')
-            new_dna+= dna.charAt(i);
-        else
-        {
-            System.out.println( "The dna sequence is wrong");
-            System.exit(0);
-        }
+        String new_dna = "";
+        for (int i = dna.length() - 1; i >= 0; i--) {
+            if (dna.charAt(i) == 't' || dna.charAt(i) == 'a' || dna.charAt(i) == 'g' || dna.charAt(i) == 'c' || dna.charAt(i) == 'A' || dna.charAt(i) == 'C' || dna.charAt(i) == 'G' || dna.charAt(i) == 'T')
+                new_dna += dna.charAt(i);
+            else {
+                System.out.println("The dna sequence is wrong");
+                System.exit(0);
+            }
         }
         return new_dna;
     }

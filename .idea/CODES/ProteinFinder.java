ff33cf33cd0e7864231cdbfd8b5506a5fd9b3c1d
Rhea-Roxy <79743359+Rhea-Roxy@users.noreply.github.com>
2021-11-03 20:52:13 -0400

    New

--- .idea/CODES/ProteinFinder.java
+++ .idea/CODES/ProteinFinder.java
@@ -1,16 +1,22 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class ProteinFinder
-{
-    public static void main(String[] args) {
+public class ProteinFinder {
+    public static void main(String[] args) throws FileNotFoundException {
         ProteinFinder s = new ProteinFinder();
-        System.out.println("Enter the DNA Sequence:  ");
-        Scanner sc = new Scanner(System.in);
-        String dna = sc.next();
+        String dna = "";
+        Scanner sc = new Scanner(new File("C:\\Users\\nehar\\IdeaProjects\\com.company\\src\\com\\company\\M13mp18.txt"));
+
+        while (sc.hasNextLine()) {
+            dna += sc.nextLine();
+        }
         System.out.println(s.length(dna));
     }
+
+
     public int length(String dna) {
 
         boolean start = false;
@@ -19,19 +25,19 @@ public class ProteinFinder
         for (int i = 0; i < dna.length() - 2; i++) {
 
             String codon = String.valueOf(dna.charAt(i)) + String.valueOf(dna.charAt(i + 1)) + String.valueOf(dna.charAt(i + 2));
-            if (codon.equals("ATG")) {
+            if (codon.equals("ATG") || codon.equals("atg")) {
                 start = true;// once it hits the start codon it should be in sets of threes
-                i+=2;
+                i += 2;
 
-            } else if (codon.equals("TAG") || codon.equals("TGA") || codon.equals("TAA")) {
+            } else if (codon.equals("TAG") || codon.equals("TGA") || codon.equals("TAA") || codon.equals("tag") || codon.equals("tga") || codon.equals("taa")) {
                 end = true;
             }
-            if (start && !end ) {
+            if (start && !end) {
                 count++;
             }
         }
-        if (start && end )
-            return count-1;
+        if (start && end)
+            return count - 1;
         else
             return -1;
     }
